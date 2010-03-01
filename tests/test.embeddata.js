e769883cdf361d2b8ce8ee983cffe6b94070c3d0
Gabriel Falcão <gabriel@nacaolivre.org>
2010-03-01 09:43:17 -0300

    Comitting the very basic features

--- tests/test.embeddata.js
+++ tests/test.embeddata.js
@@ -1,12 +1,31 @@
-module("Core");
+module("edata");
 test('It should recover data from inline script tag.', function() {
-         expect(1);
+         expect(2);
          var data = $("div#test1").edata("get");
-         ok(data.test_data == "Greetings from test1", "The div #test1 have the attribute 'test_data'");
+         var value = $("div#test1").edata("get", "test_data");
+         equal(data.test_data, "Greetings from test1");
+         equal(value, "Greetings from test1");
      });
 
 test('It should count the data length', function() {
          expect(1);
          var number = $("div#test2").edata("total");
-         ok(number == 1, "The data count should be 1");
+         equal(number, 5, "The data count should be 5");
+     });
+
+test('It should get the raw data', function() {
+         expect(1);
+         var raw = $("div#test1").edata("raw");
+         equal(raw, '{test_data: "Greetings from test1"}');
+     });
+
+test('It should be able to update some field', function() {
+         expect(3);
+         var old = $("div#test3").edata("get", "will_be_updated");
+         $("div#test3").edata("set", "will_be_updated", "totally new value");
+         var got = $("div#test3").edata("get", "will_be_updated");
+
+         notEqual(old, got, "The new value should be different of the old one");
+         equal(got, "totally new value", "The got value should be 'totally new value'");
+         equal($("div#test3").edata("raw"), '{"will_be_updated":"totally new value"}');
      });
